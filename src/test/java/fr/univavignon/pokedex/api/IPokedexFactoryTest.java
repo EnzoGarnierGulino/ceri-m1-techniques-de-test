a843400320e8611f12c0beca3b1f2df966327d89
Enzo GARNIER-GULINO <enzogarniergulino@gmail.com>
2024-03-11 10:59:18 +0100

    Update: config.yml

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -4,27 +4,24 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class IPokedexFactoryTest {
     private IPokedexFactory pokedexFactory;
-    private IPokemonMetadataProvider metadataProvider;
-    private IPokemonFactory pokemonFactory;
 
     @Before
-    public void setUp() throws PokedexException {
+    public void setUp() {
         pokedexFactory = mock(IPokedexFactory.class);
-        metadataProvider = mock(IPokemonMetadataProvider.class);
-        pokemonFactory = mock(IPokemonFactory.class);
         IPokedex pokedex = mock(IPokedex.class);
-        when(pokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(pokedex);
+        when(pokedex.size()).thenReturn(0);
+        when(pokedexFactory.createPokedex(any(), any())).thenReturn(pokedex);
     }
 
-//    @Test
-//    public void testCreatePokedex() {
-//    IPokedex pokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
-//        assertEquals(metadataProvider, pokedex.getMetadataProvider());
-//        assertEquals(pokemonFactory, pokedex.getPokemonFactory());
-//    }
+
+    @Test
+    public void IPokedexFactoryTestTest() throws PokedexException {
+        assertEquals(pokedexFactory.createPokedex(any(), any()).size(), 0);
+    }
 }
