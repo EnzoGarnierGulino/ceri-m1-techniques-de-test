4430ec7f1e0dd1a529a171ffd20e7f86d3ebef3c
Enzo GARNIER-GULINO <enzogarniergulino@gmail.com>
2024-03-18 11:08:49 +0100

    Update: Better test coverage

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -47,4 +47,38 @@ public class IPokedexTest {
         assertEquals("Bulbasaur", sortedPokemons.get(0).getName());
         assertEquals("Ivysaur", sortedPokemons.get(1).getName());
     }
+
+    @Test
+    public void testCreatePokemon() throws PokedexException {
+        Pokemon pokemon = pokedex.createPokemon(1, 20, 30, 10, 40);
+        assertEquals("Bulbasaur", pokemon.getName());
+        assertEquals(49, pokemon.getAttack());
+        assertEquals(49, pokemon.getDefense());
+        assertEquals(45, pokemon.getStamina());
+        assertEquals(20, pokemon.getCp(), 0);
+        assertEquals(30, pokemon.getHp());
+        assertEquals(10, pokemon.getDust(), 0);
+        assertEquals(40, pokemon.getCandy(), 0);
+        assertEquals(100, pokemon.getIv(), 0);
+        assertEquals(1, pokemon.getIndex());
+    }
+
+    @Test
+    public void testGetPokemonMetadata() throws PokedexException {
+        PokemonMetadata metadata = pokedex.getPokemonMetadata(1);
+        assertEquals("Bulbasaur", metadata.getName());
+        assertEquals(49, metadata.getAttack());
+        assertEquals(49, metadata.getDefense());
+        assertEquals(45, metadata.getStamina());
+    }
+
+    @Test
+    public void testGetPokemonMetadataException() throws PokedexException {
+        try {
+            pokedex.getPokemonMetadata(-1);
+        } catch (PokedexException e) {
+            assertEquals("Pokemon not found", e.getMessage());
+            System.out.println(e.getMessage());
+        }
+    }
 }
